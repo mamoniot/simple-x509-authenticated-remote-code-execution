39b222815d145d1ad6e3a8a98e13c387b2d342f8
Monica Moniot <mamoniot@proton.me>
2025-11-06 11:56:53 -0500

    reorganized handling

--- src/crypto.h
+++ src/crypto.h
@@ -3,7 +3,7 @@
 
 typedef struct PubKey PubKey;
 
-bool pub_key_extract(byte *raw_cert, const x509Fields *fields, PubKey *ret_pub_key);
+bool extract_self_sign(byte *raw_cert, const x509Fields *fields, time_t now, PubKey *ret_pub_key);
 
 bool pub_key_verify(PubKey *pub_key, const byte *data, uinta data_size, const byte *sig,
                     uinta sig_size);
