9a769a82c750a59b96a5ff82803228e1fcc6b791
Monica Moniot <mamoniot@proton.me>
2025-11-11 17:47:43 -0500

    fixed tests on WSL and other slow systems

--- test_one.sh
+++ test_one.sh
@@ -19,11 +19,15 @@ fi
 
 ./server.exe $1/cert.der &> "$OUT" &
 pid=$!
+# Timeout to allow the server to finish opening a socket before sending data.
 sleep 0.5
 
 if pgrep server.exe &> /dev/null; then
     # Requests are sent concurrently but they complete in a definite order.
-    # This is technically a race condition if a request takes longer than 1 sec.
+    # This is technically a race condition if sending takes longer than 1 second to complete.
+    # When sending over localhost this should never happen.
+    # The only way to fully eliminate this race condition is for the server to communicate to this
+    # bash script the current status of all of its connections, which is absurd.
     ./gen_sig.exe $1/priv.key test/scripts/hello.sh $2 | nc -w 1 127.0.0.1 56544 &
     pid1=$!
     ./gen_sig.exe $1/priv.key test/scripts/empty.sh $2 | nc -w 2 127.0.0.1 56544 &
@@ -37,7 +41,10 @@ if pgrep server.exe &> /dev/null; then
     pid5=$!
 
     wait $pid1 $pid2 $pid3 $pid4 $pid5
-    sleep 0.5
+    # I discovered that WSL is so goddamn slow that 0.5 seconds of timeout here is not long enough
+    # for the test to be complete before we kill the process and start the next test. So the timeout
+    # is now 2 seconds.
+    sleep 2
     kill $pid &> /dev/null
     sleep 0.5
 fi
